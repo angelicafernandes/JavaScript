02aa71558dcda9608114ad23fd0e7b9d5abf42ad
angelicafernandes <angelicatelex@hotmail.com>
2024-06-19 16:48:59 -0300

    Add files via upload

--- aluguel.js
+++ aluguel.js
@@ -1,11 +1,14 @@
+
 const readline = require('readline-sync');
-let tipo = parseFloat(readline.question("Qual o tipo de Im ovel (Apartamento ou Casa?) "));
-let area = parseFloat(readline.question("Qual a AREA DO Imovel em metros? "));
-let aluguel = (area * 20);
-let titulo = "===== Valor do Aluguel ===== ";
-let titarea = " Area:" + area
-let valoraluguel = "Valor do Aluguel: R$" + aluguel;
+let tipoim = (readline.question("Qual o tipo de Imovel (Apartamento ou Casa?) "));
+let area = (readline.question("Qual a Area do Imovel em metros? "));
+let valorm2 = (readline.question("Qual o valor/M2? "));
+let aluguel = (area * valorm2);
+
+let titulo = ("====== VALOR DO ALUGUEL =====");
 console.log(titulo);
-console.log(titarea, valoraluguel);
-#resumo = "${tipo} - ${Area:} - $valordoaluguel: R$";
-#console.log(resumo)
+
+let resultado = `${tipoim} - Area de ${area} M2  - Valor do Aluguel:R$ ${aluguel.toFixed(2)}`;
+console.log(resultado)
+
+
