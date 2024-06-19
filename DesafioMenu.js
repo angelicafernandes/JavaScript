b77728d78ed534689a24a9eb99503ecaaba902ab
angelicafernandes <angelicatelex@hotmail.com>
2024-06-19 17:23:30 -0300

    Add files via upload

---
+++ DesafioMenu.js
@@ -0,0 +1,24 @@
+const readline = require('readline-sync');
+
+let titulo = ("========= MENU =========");
+console.log(titulo);
+
+let opcao1  = ("1 - Listar Clientes");
+console.log(opcao1);
+
+let opcao2  = ("2 - Cadastrar Cliente");
+console.log(opcao2);
+
+let opcao3  = ("3 - Editar Cliente");
+console.log(opcao3);
+
+let  opcao4 = ("4 - Remover Cliente");
+console.log(opcao4);
+
+let  opcao0 = ("0 - Sair do Sistema");
+console.log(opcao0);
+
+
+console.log("========================");
+console.log("Opcao: _____");
+console.log("========================");
